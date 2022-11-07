fd14102bd551fae18f347738e7483009cf9e871e
Peter Durham <peterdurham@protonmail.com>
2022-11-06 21:49:19 -0800

    added index.js file

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// imports the API from the routes/api folder
+const blocks = require("./routes/api/blocks");
+
+// initializes the express application
+const app = express();
+
+// sets up CORS for Cross-Origin-Resource-Sharing
+app.use(cors());
+// converts API responses to JSON for easy use
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// imports our database credentials (stored separately for security)
+const db = require("./config/keys").mongoURI;
+
+// initializes our database using the credentials
+// mongoose.set("useFindAndModify", false);
+mongoose
+  .connect(db, () => {}, { useNewUrlParser: true })
+  .then(() => console.log("Mongo Database connected"))
+  .catch((err) => console.log(err));
+
+// creates a route where we can interact with our API
+app.use("/api/blocks", blocks);
+
+// sets the port number depending if we are in production or development
+const port = process.env.PORT || 5050;
+
+// intializes the server and logs a message
+server = app.listen(port, () => console.log(`Server running on port ${port}`));
